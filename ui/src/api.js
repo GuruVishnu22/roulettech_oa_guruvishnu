ffc8235979b59d82b0527602bf13a16f0093a0bb
Guru Vishnu <brguruvishnu22@gmail.com>
2024-08-20 10:54:41 -0700

    update endpoint

--- ui/src/api.js
+++ ui/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.43.7:8080/api",
+  baseURL: "https://guruvishnu.pythonanywhere.com/api",
 });
 
 export default api;
